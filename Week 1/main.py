1a5b9ad61cbd49336369005b78acd245f47208cf
Alfbom27 <alst2104@student.miun.se>
2025-01-21 16:04:45 +0100

    update

--- Week 1/main.py
+++ Week 1/main.py
@@ -1,6 +1,11 @@
 import cv2
 import sys
 
+import numpy as np
+
+
+def do_nothing(x):
+    pass
 
 def initialize_camera(camera_index=0):
     cap = cv2.VideoCapture(camera_index)
@@ -13,8 +18,13 @@ def initialize_camera(camera_index=0):
     return cap
 
 
-def process_frame(frame):
-    # Add your processing code here
+
+
+def process_frame(frame, brightness, sharpening, edge_detection, box_blur):
+    frame = cv2.filter2D(frame, -1, sharpening)
+    frame = cv2.filter2D(frame, -1, edge_detection)
+    frame = cv2.filter2D(frame, -1, box_blur)
+    frame = cv2.convertScaleAbs(frame, alpha=1.0, beta=brightness-50)
     return frame
 
 
@@ -25,6 +35,28 @@ def main():
         sys.exit(1)
 
     print("Camera feed started. Press 'q' to quit.")
+    cv2.namedWindow('Original Frame')
+    cv2.createTrackbar('Brightness', 'Original Frame', 50, 100, do_nothing)
+    cv2.createTrackbar('Sharpening', 'Original Frame', 0, 1, do_nothing)
+    cv2.createTrackbar('Edge detection', 'Original Frame', 0, 1, do_nothing)
+    cv2.createTrackbar('Box blur', 'Original Frame', 0, 1, do_nothing)
+    cv2.resizeWindow('Original Frame', 1200, 900)
+
+    sharpening_kernel = np.array([[ 0, -1, 0],
+                                [-1, 5, -1],
+                                [0, -1, 0]])
+
+    edge_detection_kernel = np.array([[ 0, -1, 0],
+                                [-1, 4, -1],
+                                [0, -1, 0]])
+
+    box_blur_kernel = np.array([[1, 1, 1],
+                              [1, 1, 1],
+                              [1, 1, 1]])/9
+
+    identity_kernel = np.array([[0, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, 0]])
 
     while True:
         # Capture frame-by-frame
@@ -36,7 +68,15 @@ def main():
             break
 
         # Process the frame with a chosen (set) of functions
-        output_frame = process_frame(frame)
+        brightness = cv2.getTrackbarPos('Brightness', 'Original Frame')
+        apply_sharpening = cv2.getTrackbarPos('Sharpening', 'Original Frame')
+        apply_edge_detection = cv2.getTrackbarPos('Edge detection', 'Original Frame')
+        apply_blur = cv2.getTrackbarPos('Box blur', 'Original Frame')
+
+        sharpening = sharpening_kernel if apply_sharpening else identity_kernel
+        edge_detection = edge_detection_kernel if apply_edge_detection else identity_kernel
+        box_blur = box_blur_kernel if apply_blur else identity_kernel
+        output_frame = process_frame(frame, brightness, sharpening, edge_detection, box_blur)
 
         # Display the original frame
         cv2.imshow('Original Frame', frame)
